6cc7ab1a7ba91d6f28ad96d2f27e8a997879b2e6
Param Siddharth <contact@paramsid.com>
2021-07-12 11:24:59 +0530

    Improve and add better functionality

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs-extra');
 const minify = require('minify');
 const { promisify } = require('util');
 
+const SOURCE_DIR = path.join(__dirname, 'src');
+const  BUILD_DIR = path.join(__dirname, 'build');
+
 const options = {
 	html: {
 		removeAttributeQuotes: false,
@@ -18,12 +21,24 @@ const gl = promisify(glob.glob);
 
 (async () => {
 
-const matches = await gl('./src/*');
+const matches = await gl(`${SOURCE_DIR}/**/*`, { nodir: true });
 
 for (const match of matches) {
-	const filename = path.basename(match);
-	const minified = await minify(match, options);
-	await fs.writeFile(path.join(__dirname, 'build', filename), minified);
+	const file = match.substring(SOURCE_DIR.length + 1);
+	const newLocation = path.join(__dirname, 'build', file);
+	const extension = path.extname(file);
+	try {
+		let minified;
+		if (extension === '.json')
+			minified = JSON.stringify(JSON.parse(await fs.readFile(match)));
+		else
+			minified = await minify(match, options);
+		await fs.outputFile(newLocation, minified);
+		console.log(`Successfully minified: ${path.basename(file)}`);
+	} catch(e) {
+		await fs.outputFile(newLocation, await fs.readFile(match));
+		console.log(`Feiled to minify, copying as is: ${path.basename(file)}`);
+	}
 }
 
 })();
